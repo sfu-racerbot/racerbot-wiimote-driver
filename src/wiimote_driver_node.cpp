f7af93af1d9e30f528a34aa2a05c42de23851ff1
SFU Racerbot <sfuracerbot@gmail.com>
2026-08-24 17:16:32 -0700

    initial commit

---
+++ src/wiimote_driver_node.cpp
@@ -0,0 +1,15 @@
+#include "rclcpp/rclcpp.hpp"
+
+class WiimoteDriverNode : public rclcpp::Node
+{
+public:
+    WiimoteDriverNode() : Node("wiimote_driver_node")
+    {
+    }
+
+private:
+};
+
+int main()
+{
+}
